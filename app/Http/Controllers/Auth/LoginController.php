3108ffc6966ac4725a08fae251dab13dc1ca27e7
中西　洋翔 <aibou@nakanishiyoushounoiMac.local>
2020-04-19 17:08:11 +0900

    ログイン変更

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
+use RedirectsUsers, ThrottlesLogins;
 use Cookie;
 
 class LoginController extends Controller
