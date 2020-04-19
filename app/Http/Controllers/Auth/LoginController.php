bfe81d6ba3fe27966e477d4be459aec776690ea7
中西　洋翔 <aibou@nakanishiyoushounoiMac.local>
2020-04-19 16:41:33 +0900

    ログイン変更

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -27,7 +28,7 @@ class LoginController extends Controller
      */
     //protected $redirectTo = '/';
 
-    protected function redirectTo()
+    protected function redirectTo(Request $request)
     {
         if(Auth::user()->email_verified_at !== null){
             if(Cookie::get('redirectafterregister') !== null){
